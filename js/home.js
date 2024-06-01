2ad60d4c5167454f98e47f561568347afd6e91b1
Torrilla Nicolas <torrilla.nicolas@gmail.com>
2024-06-01 01:58:01 -0300

    1

--- js/home.js
+++ js/home.js
@@ -1,11 +1,11 @@
 const user = JSON.parse(localStorage.getItem("loginSucces")) || false
 if(!user){
-    window.location.href = "index.html"
+    window.location.href = "./index.html"
 }
 const logOut = document.querySelector("#logOut")
 
 logOut.addEventListener("click", ()=>{
     alert("Hasta la proxima!")
     localStorage.removeItem("loginSucces")
-    window.location.href = "index.html"
+    window.location.href = "./index.html"
 })
